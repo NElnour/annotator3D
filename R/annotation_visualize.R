d537d4d4a64a633dc6cac9a0e363d2bf7fd90215
Nada <nelnour90@gmail.com>
2019-12-03 13:34:18 -0500

    Initialize vignette directory

---
+++ R/annotation_visualize.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param classified_motifs
+#'
+#' @return
+#' @export
+#'
+#' @examples
+annotation_visualize <- function(classified_motifs){
+  # parse classified motifs list as dataframe:
+  # motif
+}
