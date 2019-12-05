1eed1e403283e3fedd7592a09617e8f45249e4a6
Nada <nelnour90@gmail.com>
2019-12-04 22:27:31 -0500

    Add sample data for shiny app

---
+++ inst/shinyApp/app.R
@@ -0,0 +1,128 @@
+library(annotator3D)
+library(shiny)
+
+gui <- fluidPage(
+  titlePanel("Classify Motifs into Chromatin Loops"),
+
+  sidebarPanel(
+    h4("Main Control"),
+    fileInput("motifs", h5("Upload matched motifs:")),
+    fileInput("chromLoops", h5("Upload predicted loops:")),
+    sliderInput(
+      "threshold",
+      "Score Threshold:",
+      min = 0,
+      max = 1,
+      value = 0
+    ),
+    textInput("genome", h5("Genome:"), value = "Enter genome"),
+    textInput("chr", h5("Choromosome no.:"), value = "Enter chromosome number"),
+    #tags$hr(),
+    h4("Gviz paramaters"),
+    textInput("file_name", h5("Motif file name:")),
+    sliderInput(
+      "from",
+      "From:",
+      min = 1,
+      max = 248999999,
+      value = 0
+    ),
+    sliderInput(
+      "to",
+      "To:",
+      min = 2,
+      max = 249000000,
+      value = 0
+    ),
+    actionButton("submit", "Submit")
+  ),
+
+  mainPanel(plotOutput("GvizLayers"))
+)
+
+server <- function(input, output) {
+  plot_gviz <- eventReactive(input$submit, {
+    plot(runif(1000))
+    req(input$motifs)
+    req(input$chromLoops)
+
+    matched_motifs <- read.delim(input$motifs$datapath,
+                                 header = TRUE,
+                                 sep = "\t",
+                                 stringsAsFactors = FALSE)
+
+    matched_motifs <- matched_motifs[complete.cases(matched_motifs), ]
+
+    check1 <- assertthat::are_equal(
+      colnames(matched_motifs),
+      c(
+        "motif_id",
+        "motif_alt_id",
+        "sequence_name",
+        "start",
+        "stop",
+        "strand",
+        "score",
+        "p.value",
+        "q.value",
+        "matched_sequence"
+      )
+    )
+
+    if (!check1) {
+      stop(
+        "Motif file must have the following columns in order: motif_id,
+        motif_alt_id, sequence_name, start, stop, strand, score, p.value,
+        q.value, matched_sequence "
+      )
+    }
+
+    loops <- read.delim(input$chromLoops$datapath,
+                        header = TRUE,
+                        sep = "\t",
+                        stringsAsFactors = FALSE)
+
+    loops <- loops[complete.cases(loops), ]
+
+    check2 <-
+      assertthat::are_equal(colnames(loops), c("chr", "start", "stop"))
+
+    if (!check2) {
+      stop("Chromatin loops TSV must have the following columns in order: chr, start, stop")
+    }
+
+    motifs.cl <- classify(matched_motifs, loops)
+
+    colnames(motifs.cl) <- c("chromosome",
+              "start",
+              "end",
+              "symbol",
+              "score")
+
+    motifs.cl <- filter_motifs(motifs.cl, thresh = input$threshold)
+
+    chrom <- unique(as.character(motifs.cl$chromosome))
+    gen <- c("mm10")
+    names(gen) <- chrom
+
+    antrack <- Gviz::GeneRegionTrack(
+      motifs.cl,
+      genome = gen,
+      chromosome = chrom,
+      name = "Mapped Features",
+      transcript = "symbol",
+      transcriptAnnotation = "symbol"
+    )
+
+    plot_layered_gviz(antrack, gen, from = input$from, to = input$to)
+
+  })
+
+  output$GvizLayers <- renderPlot({
+    plot_gviz()
+  })
+}
+
+shinyApp (gui, server)
+
+# [END]
